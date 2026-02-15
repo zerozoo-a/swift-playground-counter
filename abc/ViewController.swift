7cc6841b600c32dce23759da095abe530f215dac
mustit-yjyoon <121835139+mustit-yjyoon@users.noreply.github.com>
2026-02-15 20:33:51 +0900

    Initial Commit

---
+++ abc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  abc
+//
+//  Created by 윤영주 on 2/15/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ abcTests/abcTests.swift
@@ -0,0 +1,17 @@
+//
+//  abcTests.swift
+//  abcTests
+//
+//  Created by 윤영주 on 2/15/26.
+//
+
+import Testing
+@testable import abc
+
+struct abcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
